fd0f4c73e309a66e7108170cc041498d5cc77407
lordstevig-ops <lordstevig@gmail.com>
2026-05-02 12:54:50 -0500

    Delete ezshare-master directory

--- ezshare-master/ezshare-master/packages/electron/vite.main.config.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config
-export default defineConfig({});
--- ezshare-master/packages/web/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-
-
-// eslint-disable-next-line import/prefer-default-export
-export const Route = createFileRoute('/')({
-  beforeLoad: () => {
-    throw redirect({ to: '/dir/$dirId', params: { dirId: '/' } });
-  },
-});
